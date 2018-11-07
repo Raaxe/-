0c24671d4e7f07fcd99afe026ee155cfaa5b242a
Raaxe <mrxhm1555@gmail.com>
2018-11-07 22:31:33 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(` raaxe Bot- Script By : iRxxea`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : iiRxxea ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` ,F*uk love.. ,`,"http://twitch.tv/youtube")
+client.user.setStatus("dnd")
+});
+
+
+client.login("NDk4OTQ3NjQyMTEwNTA5MDY3.DrtiUw.NcuSV8TEL__q_nF-kHoHPWHMjNc");
